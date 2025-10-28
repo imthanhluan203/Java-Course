23b89845d05e2ed318e2a2c757c2e751d3b5d026
Nguyen Thanh Luan <imthanhluan203@gmail.com>
2025-10-28 16:55:47 +0700

    add class

---
+++ src/Lesson33/SecondClass.java
@@ -0,0 +1,9 @@
+package Lesson33;
+
+public class SecondClass {
+    public static void main(String[] args){
+        System.out.print("Hello, Luan \n");
+        System.out.print("Hello World");
+
+    }
+}
