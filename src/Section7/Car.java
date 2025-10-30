4b8cdaccafa88ade2fbc6c2d319f9713ca7326c3
Nguyen Thanh Luan <imthanhluan203@gmail.com>
2025-10-30 17:08:22 +0700

    OOP is begining

---
+++ src/Section7/Car.java
@@ -0,0 +1,13 @@
+package Section7;
+
+public class Car {
+    private String make;
+    private String model;
+    private String color;
+    private int doors;
+    private boolean convertible;
+    public void describeCar(){
+        System.out.println(doors+ "-door-"  + color + " " + make + " " + model + " " +
+                (convertible ? "Convertible" : "hehe"));
+    }
+}
---
+++ src/Section7/Main.java
@@ -0,0 +1,8 @@
+package Section7;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.describeCar();
+    }
+}
